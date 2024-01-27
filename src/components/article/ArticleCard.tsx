41f8853801abb417c73e1b77eaa79243e6674475
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-01-27 01:20:11 +0100

    Add ArticleCard component and styling

---
+++ src/components/article/ArticleCard.tsx
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import { ArticleFullType } from "../../types/article";
+
+function ArticleCard({
+  article,
+  theme,
+}: {
+  article: ArticleFullType;
+  theme: string;
+}) {
+  const { name, category, author, date, image, urls, color, description } =
+    article;
+  const navigate = useNavigate();
+
+  const lastDate = new Date(
+    date[date.updatedAt ? "updatedAt" : "createdAt"]
+  ).toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const displayDate = date.updatedAt
+    ? `Mis à jour le ${lastDate}`
+    : `Créé le ${lastDate}`;
+
+  const gradientColor =
+    theme === "light"
+      ? `linear-gradient(to top right,${color}, white 150%)`
+      : `linear-gradient(to top right, ${color}, black 150%)`;
+
+  const navigateToUrl = () => {
+    return;
+    navigate(urls[0]);
+  };
+
+  return (
+    <article onClick={navigateToUrl}>
+      <div className="image" style={{ background: `${gradientColor}` }}>
+        <img src={image} />
+      </div>
+      <div className="content">
+        <div>
+          <span>{category.toUpperCase()}</span>
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+        <div>
+          <span>
+            {author} - {displayDate}
+          </span>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default ArticleCard;
--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -1,9 +1,24 @@
-// import { useAppSelector } from "../store";
+import ArticleCard from "../components/article/ArticleCard";
+import { useAppSelector } from "../store";
+import { ArticleFullType } from "../types/article";
 
 function ArticlesPage() {
-  // const articles = useAppSelector((state) => state.article.articles);
+  const articles = useAppSelector<ArticleFullType[]>(
+    (state) => state.article.articles
+  );
+  const theme = useAppSelector((state) => state.settings.theme);
 
-  return <div>Articles</div>;
+  return (
+    <div id="articles-page">
+      <div id="articles-list">
+        {articles?.length
+          ? articles.map((article, index) => (
+              <ArticleCard theme={theme} key={index} article={article} />
+            ))
+          : null}
+      </div>
+    </div>
+  );
 }
 
 export default ArticlesPage;
